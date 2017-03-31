f396846159303fed68975817020f3a00f75ef4fc
likai <likai12@baidu.com>
2017-03-31 16:19:54 +0800

    Median of Two Sorted Arrays

--- src/cn/penghaonan/Main.java
+++ src/cn/penghaonan/Main.java
@@ -2,12 +2,13 @@ package cn.penghaonan;
 
 import cn.penghaonan.problems.AddTwoNumbers;
 import cn.penghaonan.problems.LongestSubstringWithoutRepeatingCharacters;
+import cn.penghaonan.problems.MedianOfTwoSortedArrays;
 import cn.penghaonan.problems.TwoSum;
 
 public class Main {
 
     public static void main(String[] args) {
-        ITest test = new LongestSubstringWithoutRepeatingCharacters();
+        ITest test = new MedianOfTwoSortedArrays();
         test.test();
     }
 }
